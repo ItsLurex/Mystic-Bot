a2cbd7f3d5f53444fffcc297d4b3b37bb2b9562d
ItsLurex <xm600000@gmail.com>
2026-07-30 14:55:12 +0300

    Update ban.js

--- src/commands/Moderation/ban.js
+++ src/commands/Moderation/ban.js
@@ -78,7 +78,6 @@ export default {
             dmMessage,
         });
 
-        const dmNote = !notify
             ? '\n**DM:** Skipped (notify:false)'
             : result.dmSent
                 ? '\n**DM:** Sent'
