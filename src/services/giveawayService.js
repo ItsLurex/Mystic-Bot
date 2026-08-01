6231b7e53a11b9abc84645f0670ecacd8d936412
ItsLurex <xm600000@gmail.com>
2026-08-01 14:56:03 +0300

    Update giveawayService.js

--- src/services/giveawayService.js
+++ src/services/giveawayService.js
@@ -387,8 +387,7 @@ export async function checkGiveaways(client) {
         }
 
         if (winners.length > 0) {
-          const winnerAnnouncement = `🎉 Congratulations ${winnerMentions}! You won the **${giveaway.prize || 'giveaway'}**
-          Please open a ticket to claim your prize.`;
+          const winnerAnnouncement = `🎉 Congratulations ${winnerMentions}! You won the **${giveaway.prize || 'giveaway'}**. Make a ticket to claim your prize in 24 hours`;
           const winnerPingMsg = await channel.send({ content: winnerAnnouncement });
           giveaway.winnerPingMessageId = winnerPingMsg.id;
           await markGiveawayEnded(client, giveawayId, giveaway);
